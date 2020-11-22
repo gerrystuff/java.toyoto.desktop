a9e41f4dab31150e3f464fb30d979d977703786d
Gerardo Padilla <luisgerardoaguilarpadilla@gmail.com>
2020-11-22 02:32:01 -0700

    pff..

---
+++ src/HILOS/TESTING/test.java
@@ -0,0 +1,21 @@
+package HILOS.TESTING;
+
+class linea extends Thread{
+
+        }
+
+        class estacion extends Thread{
+
+        }
+public class test {
+
+    public static void main(String[] args) {
+
+        Thread matriz [][]= new Thread[2][3];
+
+        for(int i = 0; i < 2; i++){
+            for(int k = 0; k < 3; ++)
+                matriz[i][k] =
+        }
+    }
+}
